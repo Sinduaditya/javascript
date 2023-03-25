5f005f1977ca9b754a6cc3f444192f2019b90758
SinduAditya <nduujanadi51@gmail.com>
2023-03-25 11:18:20 +0700

    javascript

--- AsynchronousProcess/promisestaticmethod.js
+++ AsynchronousProcess/promisestaticmethod.js
@@ -35,3 +35,57 @@ const promrace3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
 Promise.race([promrace1, promrace2, promrace3])
     .then((values) => console.log(values))
     .catch((error) => console.log(error.message));
+
+// $ Promise.allSettled()
+/*
+  Promise.allSettled() bekerja mirip seperti Promise.all(). Namun, Promise.allSettled() mengembalikan seluruh hasil dari Promise yang dieksekusi dalam bentuk array of object dengan format beriku
+  Dari struktur objek yang dihasilkan, Anda bisa melihat bahwa Promise.allSettled() akan mengembalikan seluruh nilai Promise yang dijalankan, baik yang statusnya fulfilled ataupun rejected.
+*/
+const promiseallSet1 = new Promise((resolve) =>
+    setTimeout(() => resolve(1), 1000)
+);
+const promiseallSet2 = new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error("Error")), 2000)
+);
+const promiseallSet3 = new Promise((resolve) =>
+    setTimeout(() => resolve(3), 3000)
+);
+
+Promise.allSettled([promiseallSet1, promiseallSet2, promiseallSet3]).then(
+    (result) => console.log(result)
+);
+
+// # Promise.any()
+/*
+  Method Promise terakhir yang akan kami bahas adalah Promise.any(). Cara kerja method ini mirip seperti Promise.race(), tetapi hanya mengembalikan nilai tercepat yang berstatus fulfilled. Jika seluruh Promise berstatus rejected, method ini akan mengembalikannya dengan pesan “All Promises were rejected”.
+*/
+
+// fulfilled sample
+const promiseResolve1 = new Promise((resolve, reject) =>
+    setTimeout(() => resolve("1"), 1000)
+);
+const promiseResolve2 = new Promise((resolve, reject) =>
+    setTimeout(() => resolve("2"), 2000)
+);
+const promiseResolve3 = new Promise((resolve, reject) =>
+    setTimeout(() => resolve("3"), 3000)
+);
+
+Promise.any([promiseResolve1, promiseResolve2, promiseResolve3])
+    .then((value) => console.log(value)) // 1
+    .catch((error) => console.log(error.message));
+
+// rejected sample
+const promiseReject1 = new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error("1")), 1000)
+);
+const promiseReject2 = new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error("2")), 2000)
+);
+const promiseReject3 = new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error("3")), 3000)
+);
+
+Promise.any([promiseReject1, promiseReject2, promiseReject3])
+    .then((value) => console.log(value))
+    .catch((error) => console.log(error.message)); // All Promises were rejected
